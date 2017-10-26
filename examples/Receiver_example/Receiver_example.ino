3ad4fe752952487bfcaaf810a27ddc2c343c2b60
simondlevy <simon.d.levy@gmail.com>
2017-10-26 15:17:12 -0400

    Uses floating-point SBUS::readCal() instead of integer version

--- examples/Receiver_example/Receiver_example.ino
+++ examples/Receiver_example/Receiver_example.ino
@@ -1,26 +1,26 @@
 /*
-Receiver_example.ino
-Simon D. Levy
-simon.d.levy@gmail.com
-2017-10-26
+   Receiver_example.ino
+   Simon D. Levy
+   simon.d.levy@gmail.com
+   2017-10-26
 
-Copyright (c) 2016 Bolder Flight Systems
+   Copyright (c) 2016 Bolder Flight Systems
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-and associated documentation files (the "Software"), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
-sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+   and associated documentation files (the "Software"), to deal in the Software without restriction, 
+   including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+   sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+   furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or 
-substantial portions of the Software.
+   The above copyright notice and this permission notice shall be included in all copies or 
+   substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+   BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+   DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 // This example reads an SBUS packet from an
 // SBUS receiver (FrSky XM) and reports the
@@ -32,25 +32,32 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 // serial port 1
 SBUS x8r(Serial1);
 
-// channel, fail safe, and lost frames data
-uint16_t channels[16];
-uint8_t failSafe;
-uint16_t lostFrames = 0;
-
 void setup() {
 
-  // begin the SBUS communication
-  x8r.begin();
+    // begin the SBUS communication
+    x8r.begin();
 
-  // begin serial-monitor communication
-  Serial.begin(115200);
+    // begin serial-monitor communication
+    Serial.begin(115200);
 }
 
 void loop() {
 
-  // look for a good SBUS packet from the receiver
-  if(x8r.read(&channels[0], &failSafe, &lostFrames)){
-    Serial.println(channels[0]);
-  }
+    uint8_t failSafe;
+    uint16_t lostFrames = 0;
+    float channels[16];
+
+    // look for a good SBUS packet from the receiver
+    if(x8r.readCal(&channels[0], &failSafe, &lostFrames)){
+
+        // First four channels (Throttle, Aieleron, Elevator, Rudder) are enough to see whether it's working
+        Serial.print(channels[0]);
+        Serial.print(" ");
+        Serial.print(channels[1]);
+        Serial.print(" ");
+        Serial.print(channels[2]);
+        Serial.print(" ");
+        Serial.println(channels[3]);
+    }
 }
 
